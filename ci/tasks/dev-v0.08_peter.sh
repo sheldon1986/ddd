9f060c5fb927af0c9d01d5a759bc2870844c2d3a
root <root@vmhost02>
2019-01-30 00:50:57 +0000

    test

--- ci/tasks/dev-v0.08_peter.sh
+++ ci/tasks/dev-v0.08_peter.sh
@@ -21,7 +21,7 @@ fi
 git config --global user.email "${GIT_EMAIL}"
 git config --global user.name "${GIT_NAME}"
 
-cd repo-target
+cd repo-stg
 TARGET_BRANCH=$(git branch --contains | grep -v '('| sed 's/^\**[[:blank:]]*//g'| head -n 1)
 echo "merge target branch:${TARGET_BRANCH}"
 cd ..
@@ -30,7 +30,7 @@ cd out
 shopt -s dotglob
 rm -rf *
 mv -f ../repo/* ./
-GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" git remote add -f repo-target ../repo-target
+GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" git remote add -f repo-stg ../repo-stg
 
 CURRENT_BRANCH=$(git branch --contains | grep -v '('| sed 's/^\**[[:blank:]]*//g'| head -n 1)
 echo "current branch:${CURRENT_BRANCH}"
@@ -49,4 +49,4 @@ fi
 
 echo "MESSAGE=${MESSAGE}"
 echo "MERGE_MODE=${MERGE_MODE}"
-git merge ${MERGE_MODE} "repo-target/${TARGET_BRANCH}" -m "${MESSAGE}"
+git merge ${MERGE_MODE} "repo-stg/${TARGET_BRANCH}" -m "${MESSAGE}"
